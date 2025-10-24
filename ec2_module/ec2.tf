abf137d3c2baaa728f0a382823b19595a26c7c97
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-24 15:57:11 +0530

    index

--- ec2_module/ec2.tf
+++ ec2_module/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "this"{
-    count = length(var.num_instance)
+    count = var.num_instance
     ami = try(data.aws_ami.ami.id, var.ami_id)
     instance_type = var.instance_type
     vpc_security_group_ids = var.sg_ids
     tags= merge(
         local.base_tags,{
-            "Name" = "${local.common_name_suffix}-${each.key}"
+            "Name" = "${local.common_name_suffix}-${count.index}"
         }
         
     )
