8a104a95a112ab914d1308b10eef00507b2d61d3
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-24 16:06:39 +0530

    data

--- ec2_module/data.tf
+++ ec2_module/data.tf
@@ -1,20 +1,14 @@
-data "aws_ami" "ami" {
-  executable_users = ["self"]
-  most_recent      = true
-  owners           = ["self"]
+data "aws_ami" "ami_2023" {
+  most_recent = true
+  owners      = ["amazon"] # The owner alias for official Amazon AMIs
 
   filter {
     name   = "name"
-    values = ["devops-*"]
+    values = ["al2023-ami-*-x86_64"] # Filter for AL2023 AMIs, adjust for arm64 if needed
   }
 
   filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name   = "architecture"
+    values = ["x86_64"] # Specify the desired architecture
   }
 }
--- ec2_module/ec2.tf
+++ ec2_module/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this"{
     count = var.num_instance
-    ami = try(data.aws_ami.ami.id, var.ami_id)
+    ami = try(data.aws_ami.ami_2023.id, var.ami_id)
     instance_type = var.instance_type
     vpc_security_group_ids = var.sg_ids
     tags= merge(
