dd69d273defa30fb6ede698e82f53bd29dc5de7e
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-25 11:50:44 +0530

    var

--- sg_module/variables.tf
+++ sg_module/variables.tf
@@ -10,20 +10,16 @@ variable "description"{
 
 variable "vpc_id" {
   type = string
-  default = {}
 }
 
 variable "sg_tags" {
   type = map 
-  default = {}
 }
 
 variable "project_name" {
   type = string
-  default = {}
 }
 
 variable "environment" {
   type = string
-  default = {}
 }
