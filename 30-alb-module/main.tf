8865427a5f9d3e23c161c4e21fc25aa5ee7a1fed
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-27 16:43:24 +0530

    fix

--- 30-alb-module/main.tf
+++ 30-alb-module/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "frontend_lb" {
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
   security_groups    = [var.security_groups]
-  subnets            = [var.subnets]
+  subnets            = var.subnets
 
   enable_deletion_protection = var.enable_deletion_protection
 
