1047e19c6a92454e8fbb3f79bbaf5fadf7da0ce0
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-29 09:34:51 +0530

    update

--- 30-alb-module/main.tf
+++ 30-alb-module/main.tf
@@ -31,18 +31,3 @@ resource "aws_lb_target_group" "alb-tg" {
   )
 }
 
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.frontend_lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
-  }
-}
