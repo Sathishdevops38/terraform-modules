058ee4ad1e2aaf5d6fd18c4e8b6100a5f1a572d4
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-29 16:08:52 +0530

    ami update

--- 20-bastion-module/data.tf
+++ 20-bastion-module/data.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "ami_2023" {
   most_recent = true
-  owners      = ["amazon"] # The owner alias for official Amazon AMIs
+  owners      = ["self"] # The owner alias for official Amazon AMIs
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"] # Filter for AL2023 AMIs, adjust for arm64 if needed
+    values = ["devops-practice-ami"] # Filter for AL2023 AMIs, adjust for arm64 if needed
   }
 
   filter {
