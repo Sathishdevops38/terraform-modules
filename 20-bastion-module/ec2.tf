14ac0e44aba06ae8fec55fa88e38eae19226da37
sathishdevops38 <sathishdevops38@gmail.com>
2025-10-29 15:59:14 +0530

    ami

--- 20-bastion-module/ec2.tf
+++ 20-bastion-module/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
-    ami = try(data.aws_ami.ami_2023.id, var.ami_id)
+    ami = try(var.ami_id, data.aws_ami.ami_2023.id)
     instance_type = var.instance_type
     vpc_security_group_ids = var.sg_ids
     subnet_id = var.subnet_id
