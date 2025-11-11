6483527bc8b56dc0a1125b5848d1ca7de1044943
sathishdevops38 <sathishdevops38@gmail.com>
2025-11-11 14:31:48 +0530

    fix

--- 30-alb-module/ouputs.tf
+++ 30-alb-module/ouputs.tf
@@ -5,8 +5,10 @@ output "alb_arn" {
   value = aws_lb.this.arn
 }
 
-output "alb_zone_id"{
-  value = aws_lb.this.zone_id.id
+output "alb_zone_id" {
+  # Change 'aws_lb.backend_alb' to match the actual resource name in the module
+  value       = aws_lb.this.zone_id 
+  description = "The Hosted Zone ID of the Application Load Balancer."
 }
 output "alb_dns_name" {
   value = aws_lb.this.dns_name
