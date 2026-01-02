1d270981b6b93d17f5caf2e989774ade1dc22f91
sathishdevops38 <sathishdevops38@gmail.com>
2026-01-02 10:09:57 +0530

    outputs of rt

--- 00-vpc-module/data.tf
+++ 00-vpc-module/data.tf
@@ -13,3 +13,32 @@ data "aws_route_table" "main" {
     values = ["true"]
   }
 }
+
+#to get default vp
+data "aws_vpc" "default_vpc" {
+  default = true
+
+  # Optional: Add a second filter just to be explicit
+  filter {
+    name   = "is-default"
+    values = ["true"]
+  }
+}
+
+#to get default subnet
+data "aws_subnet" "default_subnet" {
+  vpc_id     = data.aws_vpc.default_vpc.id
+  default_for_az = true
+  availability_zone = "us-west-2a"
+}
+
+#to get default security group
+data "aws_security_group" "default_sg" {
+  vpc_id = data.aws_vpc.default_vpc.id
+
+  # The filter targets the SG named 'default' in the VPC.
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
--- 00-vpc-module/ouputs.tf
+++ 00-vpc-module/ouputs.tf
@@ -22,4 +22,15 @@ output "igw_id" {
   value = aws_internet_gateway.this.id
 }
 
+output "public_rt"{
+  value = aws_route_table.public.id
+}
+
+output "private_rt"{
+  value = aws_route_table.private.id
+}
+output "database_rt" {
+  value = aws_route_table.database.id
+}
+
 
